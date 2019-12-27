aea13b2f81a6e46b7ce23ad9e0b3d257aa8eae37
Colin Mason <colinpetermason@gmail.com>
2019-12-27 10:52:38 -0800

    Intro var

---
+++ intro_to_var.R
@@ -0,0 +1,21 @@
+# integer
+x <- 2L
+# Adding an L makes it an integer, the default is floats(double)
+typeof(x)
+
+# float(double)
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+# logical 
+q1 <- T
+typeof(q1)
+q2 <- FALSE
