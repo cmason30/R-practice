412e3ea5d6837148a66cf55315abe74c983de5bc
Colin Mason <colinpetermason@gmail.com>
2019-12-30 13:46:19 -0800

    vectors

---
+++ vectorized_operations.R
@@ -0,0 +1,33 @@
+v1 <- rnorm(5)
+v1
+
+for (i in v1){
+  print(i)
+}
+
+print(v1[1])
+print(v1[2])
+
+for(j in 1:5){
+  print(v1[j])
+}
+
+# j and i loops have same outcomes but you can see they do it different ways
+
+
+
+n <- 10000000000
+
+a <- rnorm(n)
+b <-rnorm(n)
+
+# Vectorized apporach
+
+c <- a * b
+
+# De-vectorized approach
+
+d <- rep(NA, n)
+for(i in 1:n){
+  d[i] <- a[i] * b[i]
+}
---
+++ vectors_practice.R
@@ -0,0 +1,19 @@
+
+
+vector_1 <- c(3, 45, 56, 732)
+
+is.numeric(vector_1)
+
+is.integer(vector_1)
+
+vector_2 <- c(1L, 2L, 3L, 4L)
+
+seq(1,15,2)
+z <- seq(1,15,4)
+z
+
+rep(3, 50)
+d <- rep(3,250)
+d
+x <- c(80, 20, 10)
+rep(x, 10)
